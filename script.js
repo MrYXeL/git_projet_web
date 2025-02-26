775a7b3e8b3e6b041924c18f3a44990dc90277b6
MrYXeL <alexydomanski@gmail.com>
2025-02-26 16:37:22 +0100

    -

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let couleur = ["rouge", "bleu", "vert", "jaune", "blanc", "orange"]
+
 function suite_aleatoire{
-	Math.random()
+	let suite;
+	for (var i = 0; i >= 5; i++) {
+		suite[i] = Math.random(0, 5);
+	}
+	return suite
 }
+
+console.log(suite_aleatoire())
+//Rouge Bleu Vert Jaune Blanc Orange
