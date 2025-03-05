e4fb05d53e38f7dd931b5cb9341a52043ebbd4c5
MrYXeL <alexydomanski@gmail.com>
2025-03-05 20:41:54 +0100

    -

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var couleurs = ["rouge", "bleu", "vert", "jaune"];
+const couleurs = ["rouge", "bleu", "vert", "jaune"];
+const son = {"rouge": new Audio("sons/re.mp3"), "bleu": new Audio("sons/fa.mp3"), "vert": new Audio("sons/do.mp3"), "jaune": new Audio("sons/mi.mp3")};
 
 function couleur_aleatoire(){
 	return couleurs[Math.floor(Math.random() * 4)];
@@ -6,8 +7,31 @@ function couleur_aleatoire(){
 
 //console.log(couleur_aleatoire());		//Test la couleur aléatoire
 
-function on_continue(){
-	null;
+function on_continue(suite){
+	//Montre la suite au joueur
+	for(var i = 0; i < suite.length; i++){
+		if (suite[i] == "rouge"){
+			document.getElementById("rouge").style.backgroundColor = "rgb(255,80,80)";
+			son["rouge"].play();
+			setTimeout(function(){document.getElementById("rouge").style.backgroundColor = "rgb(250,0,0)"},1000);
+
+		}
+		else if (suite[i] == "bleu"){
+			document.getElementById("bleu").style.backgroundColor = "rgb(150, 150, 255)";
+			son["bleu"].play();
+			setTimeout(function(){document.getElementById("rouge").style.backgroundColor = "rgb(0, 0, 255)"},1000);
+		}
+		else if (suite[i] == "vert"){
+			document.getElementById("vert").style.backgroundColor = "rgb(150, 255, 150)";
+			son["vert"].play();
+			setTimeout(function(){document.getElementById("rouge").style.backgroundColor = "rgb(0, 255, 0)"},1000);
+		}
+		else if (suite[i] == "jaune"){
+			document.getElementById("jaune").style.backgroundColor = "rgb(255,255,150)";
+			son["jaune"].play();
+			setTimeout(function(){document.getElementById("rouge").style.backgroundColor = "rgb(250,255,0)"},1000);
+		}
+	};
 }
 
 function jeu(){
@@ -17,5 +41,13 @@ function jeu(){
 		couleur = couleur_aleatoire();
 		suite.push(couleur);
 		console.log(suite);
+		jeu_en_cour = on_continue(suite);
 	};
 }
+
+//jeu();
+
+
+
+//pour sleep() on peut faire avec datetime (prendre le time de maintenant et le comparer avec le time de maintenant + le temps qu'on veut attendre)
+//voir setIntervall()
